80c76af41d62312f97ec8e39f42c044566802c94
qidizi <qidizi@qq.com>
2015-05-08 10:28:08 +0800

    精简日志;精确提示;

--- mysql-backup-by-root-crontab.sh
+++ mysql-backup-by-root-crontab.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# 最新版本
 # https://github.com/qidizi/linux-shells/blob/master/mysql-backup-by-root-crontab.sh
-# 请把本文件加入root的tabcron,比如:
+
+# 可以使用以下命令在后台测试:
+# xxxxx.sh & 
+
+# 配置root的定时任务crontab,比如:
 # 每天03点自动备份mysql数据库
 # 0 03 * * * /home/backup/mysql-backup-by-root-crontab.sh
 
@@ -61,27 +66,30 @@ smtpHost="smtp://smtp.qq.com:25"
 
 
 
-
 SH_START=$(date +%s)
 shName=$(basename $0)
 shLogPath="/var/log/${shName}.log"
-echo -e "By ${0} @ $(date "+%F %T")\n" > $shLogPath
+echo -e "${0}@$(date "+%F %T")\n" > $shLogPath
 
 function myExit(){
     exitCode=$1
-    SH_TIME=$(expr $(date +%s) - $SH_START)
-    appendLog "脚本耗时${SH_TIME}秒"
-    appendLog "服务器IP信息:\n$(ip -4 -o addr 2>&1)"
+    appendLog "主机IP信息:\n$(ip -4 -o addr 2>&1)"
+    appendLog "脚本总耗时$(expr $(date +%s) - ${SH_START})秒"
+	
+	if [ "${exitCode}" -ne "0" ];then
+		appendLog "异常退出,请根据日志解决问题"
+	fi
+	
     ver=$(mailx -V 2>&1)
 
     if [ "$?" -ne "0" ];then
-        appendLog "用来通过smtp发送email的命令mailx无法使用,请安装,如centos使用yum install mailx,注意必须是 Heirloom mailx这个版本,跳过发送email通知的步骤,正确安装mailx后就可以收到email通知了,测试时出错信息:${ver}"
+        appendLog "发送email通知需安装Heirloom mailx，出错信息:${ver}"
     else
         mailStart=$(date +%s)
         #发送email
         mailInfo=$(cat ${shLogPath} | mailx -v -s "${smtpSubject}" -S from=${smtpFrom}  -S smtp-auth=login -S smtp=${smtpHost} -S smtp-auth-user=${smtpUser} -S smtp-auth-password="${smtpPwd}" ${smtpTo} 2>&1)
         # 无法附加发送过程的日志给email通知中,所以,只能保存到日志中,如果需要了解email的交互过程,请到日志文件中查看
-        appendLog "使用mailx通过smtp发送email的耗时$(expr $(date +%s) - $mailStart)秒;交互记录如下:\n\n${mailInfo}"
+        appendLog "发送email通知耗时$(expr $(date +%s) - ${mailStart})秒;交互记录如下:\n\n${mailInfo}"
     fi
 
     exit $exitCode
@@ -93,16 +101,15 @@ function appendLog(){
 }
 
 if [ ! -e "${backupRoot}" ];then
-    appendLog "备份根目录 ${backupRoot} 不存在,尝试创建"
     mkInfo=$(mkdir -p $backupRoot 2>&1)
 
     if [ "$?" -ne "0" ];then
-        appendLog "创建目录 ${backupRoot} 失败:${mkInfo}"
+        appendLog "创建不存在的mysql备份总目录${backupRoot}:失败,${mkInfo}"
         myExit 1
     fi
 
 elif [ ! -d "${backupRoot}" ];then
-    appendLog "备份根目录 ${backupRoot} 已经存在,但是它并非是一个目录,终止"
+    appendLog "mysql备份路径${backupRoot}虽存在,但它不是目录"
     myExit 2
 fi
 
@@ -113,47 +120,46 @@ if [ ! -e "${todayRoot}" ];then
     mkInfo=$(mkdir $todayRoot 2>&1)
 
     if [ "$?" -ne "0" ];then
-        appendLog "尝试创建本轮的备份目录 ${todayRoot} 失败,终止:${mkInfo}"
+        appendLog "创建本轮的备份目录${todayRoot}:失败,${mkInfo}"
         myExit 3
     fi
 fi
 
-appendLog "本轮的备份主目录:${todayRoot}"
 ver=$(mysql --version 2>&1)
 
 if [ "$?" -ne "0" ];then
-    appendLog "测试用来连接数据库的命令mysql的版本时出错,终止:${ver}"
+    appendLog "mysql命令异常:${ver}"
     myExit 4
 fi
 
 ver=$(mysqldump -V 2>&1)
 
 if [ "$?" -ne "0" ];then
-    appendLog "测试mysqldump命令出错,终止:${ver}"
+    appendLog "mysqldump命令异常:${ver}"
     myExit 5
 fi
 
 ver=$(tail --version 2>&1)
 
 if [ "$?" -ne "0" ];then
-    appendLog "测试tail命令时出错,终止:${ver}"
+    appendLog "tail命令异常:${ver}"
     myExit 6
 fi
 
 ver=$(tar  --version 2>&1)
 
 if [ "$?" -ne "0" ];then
-    appendLog "测试tar命令时出错,终止:${ver}"
+    appendLog "tar命令异常:${ver}"
     myExit 7
 fi
 
 databases=$(mysql --host=127.0.0.1 --user=${mysqlBackupUser}  --password="${mysqlBackupPwd}" --execute="show databases;"  --silent --skip-column-names --unbuffered  2>&1)
 
 if [ "$?" -ne "0" ]; then
-    appendLog "尝试使用配置信息列举出mysql的全部数据库名称时出错,终止:${databases}"
+    appendLog "列举全部数据库名称异常:${databases}"
     myExit 8
 else
-    appendLog "全部的数据库名称列表如下:\n${databases}"
+    appendLog "数据库全部列表:\n${databases}"
 fi
 
 
@@ -163,7 +169,7 @@ for database in $databases; do
 
     # 属于不需要备份的库
     if [ "$?" -eq "0" ];then
-        appendLog "数据库 ${database} 被指定不需要备份,跳过"
+        appendLog "${database}库指定不备份"
         continue
     fi
 
@@ -173,7 +179,7 @@ for database in $databases; do
         mkInfo=$(mkdir $databaseRoot 2>&1)
 
         if [ "$?" -ne "0" ];then
-            appendLog "尝试创建数据库 ${databaseRoot} 的备份目录失败,终止:${mkInfo}"
+            appendLog "创建${databaseRoot}库的备份目录异常:${mkInfo}"
             myExit 9
         fi
     fi
@@ -181,68 +187,59 @@ for database in $databases; do
     tables=$(mysql --host=127.0.0.1 --user="${mysqlBackupUser}"  --password="${mysqlBackupPwd}" --execute="show tables from \`${database}\`;"  --silent --skip-column-names --unbuffered 2>&1)
 
         if [ "$?" -ne "0" ]; then
-                appendLog "尝试使用配置信息列举数据库 ${database} 全部的表名时出错,终止:${tables}"
+                appendLog "列举${database}库 全部表名异常:${tables}"
                 myExit 10
         else
-                appendLog "数据库${database}全部表的列表如下:\n${tables}"
+                appendLog "${database}库的全部表名:\n${tables}"
         fi
 
     for table in $tables; do
-		#忽略备份文件
-		echo $notBackupTables|grep -i "(${database} ${table})" 2>&1 >/dev/null
+                #忽略备份文件
+                echo $notBackupTables|grep -i "(${database} ${table})" 2>&1 >/dev/null
 
-		# 属于不需要备份的库
-		if [ "$?" -eq "0" ];then
-			appendLog "数据库 ${database} 下的 ${table} 表被指定不需要备份,跳过"
-			continue
-		fi
-	
-        sqlPath="${databaseRoot}${table}.sql"
+                # 属于不需要备份的库
+                if [ "$?" -eq "0" ];then
+                        appendLog "${database}库${table}表指定不备份"
+                        continue
+                fi
+
+        sqlFile="${table}.sql"
+        sqlPath="${databaseRoot}${sqlFile}"
         timeStart=$(date +%s)
         dumpInfo=$(mysqldump --host=127.0.0.1 --user=${mysqlBackupUser} --password="${mysqlBackupPwd}" --dump-date --comments --quote-names --result-file=${sqlPath} --quick  --databases ${database} --tables ${table} 2>&1)
 
         if [ "$?" -ne "0" ];then
-            appendLog "尝试使用mysqldump导出数据库${database}中的表 ${table} 失败,终止:${dumpInfo}"
+            appendLog "mysqldump导出${database}库${table}表异常:${dumpInfo}"
             myExit 11
+        fi
+
+        sok="${database}库${table}表dump到${sqlPath}成功:耗时$(expr $(date +%s) - ${timeStart})秒;查找dump成功的'Dump completed'字符标志:"
+        tail --lines=10 "${sqlPath}" |grep "\-\- Dump completed" 2>&1 > /dev/null
+
+        if [ "$?" -ne "0" ];then
+                sok="${sok}无，请登录ssh确认本备份情况"
         else
-            appendLog "数据库 ${database}的表 ${table} dump到文件 ${sqlPath} 成功:${dumpInfo}; 耗时$(expr $(date +%s) - ${timeStart})秒"
+                sok="${sok}存在，据此判断备份成功了"
+                tarFile="${sqlFile}.tar.bz2"
+                timeStart=$(date +%s)                sok="${sok};打包压缩${sqlFile}(成功后删除之)成${tarFile}:"
+                tarInfo=$(tar --create --remove-files --bzip2 --absolute-names --directory="${databaseRoot}"   --add-file="${sqlFile}" --file="${databaseRoot}${tarFile}")
 
-            tail --lines=10 "${sqlPath}" |grep "\-\- Dump completed" 2>&1 > /dev/null
+                if [ "$?" -ne "0" ];then 
+					sok="${sok}出错,${tarInfo}"
+                else
+					sok="${sok}成功"
+                fi
 
-            if [ "$?" -ne "0" ];then
-                appendLog "dump文件${sqlPath}没有结束行用来表示成功dump的 'Dump completed' 标志字符,请登录ssh查看此文件是否备份成功"
-            else
-                appendLog "dump文件${sqlPath}从文件内容中检测到成功备份的标志字符'Dump completed',自动判断应该备份成功了"
-            fi
+                sok="${sok},耗时$(expr $(date +%s) - ${timeStart})秒;"
 
         fi
 
+        appendLog "${sok}"
     done
 
 done
 
 appendLog "\n ------数据库备份操作全部完成------\n"
-
-# 开始压缩,把压缩放到备份结束是防止压缩时间过长,如果出现锁表,会影响网站运行
-sqls=$(ls --almost-all --ignore-backups --indicator-style=slash -1 ${todayRoot}*/*.sql 2>&1)
-
-for path in $sqls; do
-    sqlDir=$(dirname $path)
-    sql=$(basename $path)
-    timeStart=$(date +%s)
-    tarInfo=$(tar --create --remove-files --bzip2 --absolute-names --directory="${sqlDir}"   --add-file="${sql}" --file="${path}.tar.bz2")
-
-    if [ "$?" -ne "0" ];then
-        appendLog "压缩出错:\n${tarInfo}"
-    else
-        appendLog "压缩成功并删除源文件"
-    fi
-
-        appendLog "压缩耗时$(expr $(date +%s) - ${timeStart})秒"
-	
-done
-
-appendLog "------压缩操作全部完成-----"
 #开始清理大于x天的备份
 
 daysDir=$(ls --almost-all --ignore-backups --indicator-style=slash -1 "${backupRoot}" 2>&1)
@@ -251,7 +248,6 @@ for bkDir in $daysDir;do
     bkDir="${backupRoot}${bkDir}"
 
     if [ ! -d "${bkDir}" ];then
-        appendLog "删除过期的备份文件操作时,检测到${bkDir}不是一个目录,跳过"
         continue
     fi
 
@@ -260,7 +256,6 @@ for bkDir in $daysDir;do
     echo $dirName | grep -P "^\d{10}$" 2>&1 >/dev/null
 
     if [ "$?" -ne "0" ];then
-        appendLog "准备删除过期备份目录${bkDir}时,检测到待删除目录名不是类2015040517(年4月2日2时2)格式的10位数字,跳过"
         continue
     fi
 
@@ -269,12 +264,18 @@ for bkDir in $daysDir;do
     #如果文件时间小于这个过期时间那么就强制删除整个目录
     if [ "${dirName}" -lt "${outDay}" ];then
         rmInfo=$(rm --force --preserve-root --recursive "${bkDir}" 2>&1)
-        appendLog "检测到一个过期备份目录 ${bkDir},已经超过 ${deleteRootOutDays} 天,也就是从备份目录名称上判断,它是小于${outDay}了 ,现被强制删除,删除状态(0为成功):${?} ;删除返回信息:${rmInfo};"
+		rmOk="成功"
+		
+		if [ "$?" -ne "0" ];then
+			rmOk="失败 -- ${rmInfo}"
+		fi
+		
+        appendLog "备份目录${bkDir}超过 ${deleteRootOutDays}天(${dirName} < ${outDay}):强制删除${rmOk}"
     fi
 
 done
 
-appendLog "------完成清理过期备份文件夹操作----"
+appendLog "------删除过期备份文件夹操作完成----"
 appendLog "空间使用情况如下:\n $(df -h 2>&1)"
-appendLog "当前备份文件占用空间情况:\n $(du -hs ${todayRoot} 2>&1)"
+appendLog "本轮备份占用空间情况:\n $(du -hs ${todayRoot} 2>&1)"
 myExit 0
